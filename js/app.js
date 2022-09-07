c992e4637e3e25cff7a6b07185fe293789d5140b
aguirre-ivan <iaguirre@fi.uba.ar>
2022-09-07 03:58:04 -0300

    Revert "Agrego funcionamiento basico de carrito y localStorage"
    Rama equivocada
    
    This reverts commit 682e5009a3fc8cbd71a573e4817d4ee569c3f433.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,11 @@
-/* *****************************************************************
- *                              CLASSES
- * *****************************************************************/
-
 // Ecommerce product
 class Product {
-    constructor(productId, name, price, imgSrc, imgAlt) {
-        this.productId = productId;
+    constructor(id, name, price, imgSrc, imgAlt) {
+        this.id = id;
         this.name = name;
         this.price = price;
         this.imgSrc = imgSrc;
         this.imgAlt = imgAlt;
-        this.quantity = 1;
     }
 
     generateEcommerceCardHTML() {
@@ -20,7 +15,7 @@ class Product {
         let div = document.createElement("div");
         div.classList.add("col", "mb-5");
         div.innerHTML = `
-            <div class="card h-100 id="${this.productId}">
+            <div class="card h-100 id="${this.id}">
                 <img class="card-img-top"
                     src="${this.imgSrc}"
                     alt="${this.imgAlt}" />
@@ -45,17 +40,13 @@ class Product {
     }
 }
 
-// Products in cart
 class Cart {
-    constructor() {
-        this.cartArray = [];
+    constructor(productsArray) {
+        this.productsArray = productsArray;
     }
 
     getTotalPrice() {
         let totalPrice = 0;
-        (this.cartArray).forEach(product => {
-            totalPrice += parseFloat(product.price) * parseInt(product.quantity);
-        })
         return totalPrice;
     }
 
@@ -66,7 +57,7 @@ class Cart {
         let divCartContainer = document.createElement("div");
         divCartContainer.classList.add("col");
 
-        this.cartArray.forEach(product => {
+        this.productsArray.forEach(product => {
             let divCard = document.createElement("div");
             divCard.classList.add("row", "justify-content-center");
             divCard.innerHTML = `
@@ -87,7 +78,7 @@ class Cart {
                                     $${product.price}
                                 </p>
                                 <p class="card-text">
-                                    Cantidad: ${product.quantity}
+                                    Cantidad: 1
                                 </p>
                                 <div class="btn-group me-2" role="group" aria-label="First group">
                                 <button type="button" class="btn btn-dark">
@@ -112,7 +103,7 @@ class Cart {
         divTotal.innerHTML = `
             <div class="row offset-8 text-end pt-4" style="max-width: 200px;">
                 <p class="h4">
-                    Total: $${this.getTotalPrice()}
+                    Total: $1200
                 </p>
                 <a href="#" class="btn btn-dark">
                     Comprar
@@ -124,165 +115,39 @@ class Cart {
 
         return divCartContainer;
     }
-
-    generateEmptyCartContainerHTML() {
-        let divCartContainer = document.createElement("div");
-        divCartContainer.classList.add("col");
-        divCartContainer.innerHTML = `
-        <section class="py-3">
-            <div class="container px-4 px-lg-5">
-                <div class="col mb-5 text-center">
-                    <div class="alert alert-primary" role="alert">
-                        ¡Tu carrito se encuentra vacío!
-                        Agregá productos en la sección de la <a href="productos.html">tienda</a>.
-                    </div>
-                </div>
-            </div>
-        </section>
-        `;
-
-        return divCartContainer;
-    }
-
-    renderCartContainerFromLocalStorage() {
-        this.updateCartArrayFromLocalStorage();
-        console.log(this.cartArray);
-
-        if (this.cartArray.length === 0) {
-            renderProductsContainer("cart-container", this.generateEmptyCartContainerHTML());
-        } else {
-            renderProductsContainer("cart-container", this.generateCartContainerHTML());
-        }
-    }
-
-    updateRenderFromLocalStorage() {
-        let container = document.getElementById("cart-container");
-        while (container.firstChild) {
-            container.removeChild(container.firstChild);
-        }
-        this.renderCartContainerFromLocalStorage();
-    }
-
-    updateCartArrayFromLocalStorage() {
-        let cartJSON = localStorage.getItem("cart");
-        let emptyArray = [];
-        return (cartJSON.length === 0) ? emptyArray : JSON.parse(cartJSON);
-    }
-
-    updateCartArrayInLocalStorage() {
-        let arrayJSON = JSON.stringify(this.cartArray);
-        localStorage.setItem("cart", arrayJSON);
-    }
-
-    addProductToCart(product) {
-        let productIsInCart = false;
-
-        (this.cartArray).forEach(cartProduct => {
-            if (cartProduct.productId === product.productId) {
-                productIsInCart = true;
-                cartProduct.quantity = parseInt(cartProduct.quantity) + 1;
-                return;
-            }
-        })
-        if (!productIsInCart) {
-            this.cartArray.push(product);
-        }
-
-        this.updateCartArrayInLocalStorage();
-        this.updateRenderFromLocalStorage();
-    }
-
-    restProductFromCart(product) {
-        for (let i = 0; i < this.cartArray.length; i++) {
-            if (this.cartArray[i].productId === product.productId) {
-                (this.cartArray[i]).quantity = parseInt(this.cartArray[i].quantity) - 1;
-                if (this.cartArray[i].quantity === 0) {
-                    this.removeProductFromCart(product);
-                }
-                break;
-            }
-        }
-
-        this.updateCartArrayInLocalStorage();
-        this.updateRenderFromLocalStorage();
-    }
-
-    removeProductFromCart(product) {
-        product.quantity = 1;
-        this.cartArray = this.cartArray.filter(cartProduct => cartProduct.productId != product.productId);
-
-        this.updateCartArrayInLocalStorage();
-        this.updateRenderFromLocalStorage();
-    }
 }
-
-/* *****************************************************************
- *                              INITIALIZATION
- * *****************************************************************/
-
-/*                              CART                               */
-
-// Initialize cart in localStorage
-if (!localStorage.getItem("cart")) {
-    localStorage.setItem("cart", []);
-}
-
-let cart = new Cart;
-
-// Render cart container from localStorage
-cart.renderCartContainerFromLocalStorage();
-
-
-/*                          ECOMMERCE                             */
 
 // Products array of ecommerce
 var productsArray = [
     new Product(
-        "P00", "Alfajores San Valentin", 800, "../assets/img/products/alfajores_san_valentin.jpg", "Caja de 6 alfajores decorados"
+        1, "Alfajores San Valentin", 800, "../assets/img/products/alfajores_san_valentin.jpg", "Caja de 6 alfajores decorados"
     ),
     new Product(
-        "P01", "Caja tentacion", 1500, "../assets/img/products/caja_tentacion.jpg", "Caja con brownies y masitas de chocolate"
+        2, "Caja tentacion", 1500, "../assets/img/products/caja_tentacion.jpg", "Caja con brownies y masitas de chocolate"
     ),
     new Product(
-        "P02", "Cheescake", 800, "../assets/img/products/cheesecake.jpg", "Torta cheesecake"
+        3, "Cheescake", 800, "../assets/img/products/cheesecake.jpg", "Torta cheesecake"
     ),
     new Product(
-        "P03", "Alfajores de maicena", 800, "../assets/img/products/alfajores_maicena.jpg", "9 alfajores de maicena apilados"
+        4, "Alfajores de maicena", 800, "../assets/img/products/alfajores_maicena.jpg", "9 alfajores de maicena apilados"
     ),
     new Product(
-        "P04", "Caja de 9 brownies decorados", 1200, "../assets/img/products/caja_brownies.jpg", "Caja de 9 brownies decorados"
+        5, "Caja de 9 brownies decorados", 1200, "../assets/img/products/caja_brownies.jpg", "Caja de 9 brownies decorados"
     ),
     new Product(
-        "P05", "Cakes", 1200, "../assets/img/products/cakes.jpg", "Caja de 3 tortas"
+        6, "Cakes", 1200, "../assets/img/products/cakes.jpg", "Caja de 3 tortas"
     ),
     new Product(
-        "P06", "Budines", 1000, "../assets/img/products/budines.jpg", "Caja de 3 budines"
+        7, "Budines", 1000, "../assets/img/products/budines.jpg", "Caja de 3 budines"
     ),
     new Product(
-        "P07", "Alfacookies", 800, "../assets/img/products/alfacookies.jpg", "Caja de 9 alfacookies"
+        8, "Alfacookies", 800, "../assets/img/products/alfacookies.jpg", "Caja de 9 alfacookies"
     ),
 ];
 
-// Render ecommerce container
-renderProductsContainer("ecommerce-container", generateEcommerceContainerHTML(productsArray));
-
-/* *****************************************************************
- *                              FUNCTIONS
- * *****************************************************************/
-
-function renderProductsContainer(productsContainerId, productsToAppend) {
-    /*
-    This functions checks if document contains the productsContainerId and then appends productsToAppend to the container finded.
-    */
-    if (document.getElementById(productsContainerId)) {
-        let cartContainer = document.getElementById(productsContainerId);
-        cartContainer.appendChild(productsToAppend);
-    }
-}
-
 function generateEcommerceContainerHTML(productsArray) {
     /*
-    This function returns an eccomerce div container with all cards product of productsArray.
+    This functions returns an eccomerce div container with all cards product of productsArray.
     */
     let productsContainerRow = document.createElement("div");
     productsContainerRow.classList.add("row", "gx-4", "gx-lg-5", "row-cols-2", "row-cols-md-3", "row-cols-xl-4", "justify-content-center");
@@ -296,3 +161,40 @@ function generateEcommerceContainerHTML(productsArray) {
     return productsContainerRow;
 }
 
+// Cart products
+let cartProductsArray = [
+    new Product(
+        1, "Alfajores San Valentin", 800, "../assets/img/products/alfajores_san_valentin.jpg", "Caja de 6 alfajores decorados"
+    ),
+    new Product(
+        2, "Caja tentacion", 1500, "../assets/img/products/caja_tentacion.jpg", "Caja con brownies y masitas de chocolate"
+    ),
+];
+let cartProducts = new Cart(cartProductsArray);
+
+
+function renderProductsContainer(productsContainerId, productsToAppend) {
+    /*
+    This functions checks if document contains the productsContainerId and then appends productsToAppend to the container finded.
+    */
+    if (document.getElementById(productsContainerId)) {
+        let cartContainer = document.getElementById(productsContainerId);
+        cartContainer.appendChild(productsToAppend);
+    }
+}
+
+// Render cart container
+renderProductsContainer("cart-container", cartProducts.generateCartContainerHTML());
+
+// Render ecommerce container
+renderProductsContainer("ecommerce-container", generateEcommerceContainerHTML(productsArray));
+
+let buttonsQuitFromCart = document.querySelectorAll(".button-quit-from-cart");
+
+for (let button of buttonsQuitFromCart) {
+    button.addEventListener("click", quitProductFromCart);
+}
+
+function quitProductFromCart(event) {
+    let productDiv = event.target.parentNode.parentNode.parentNode.parentNode.remove();
+}
